1cf362dd1ebcc74a5b2d2f0f0646a4774bbca0d3
Ashish <prasadashish478@gmail.com>
2026-03-16 19:22:00 +0530

    fix duplicate server code

--- backend/src/index.js
+++ backend/src/index.js
@@ -12,10 +12,10 @@ import callRoutes from "./routes/call.route.js";
 import { app, server } from "./lib/socket.js";
 
 dotenv.config();
-const PORT = process.env.PORT;
+const PORT = process.env.PORT || 3001;
 const __dirname = path.resolve();
 
-// 🔥 FIX: Allow large images
+// allow large images
 app.use(express.json({ limit: "50mb" }));
 app.use(express.urlencoded({ extended: true, limit: "50mb" }));
 
@@ -27,25 +27,27 @@ app.use(
       "http://localhost:5173",
       "https://real-time-communication-app-theta.vercel.app",
     ],
+    methods: ["GET","POST","PUT","DELETE","OPTIONS"],
     credentials: true,
   })
 );
+
 app.options("*", cors());
 
+// routes
 app.use("/api/auth", authRoutes);
 app.use("/api/messages", messageRoutes);
 app.use("/api/calls", callRoutes);
 
-
 if (process.env.NODE_ENV === "production") {
   app.use(express.static(path.join(__dirname, "../frontend/dist")));
-  app.get("/", (req, res) => {
-  res.send("Backend is running");
-});
 
+  app.get("/", (req, res) => {
+    res.send("Backend is running");
+  });
 }
 
 server.listen(PORT, "0.0.0.0", () => {
-  console.log("Server running on PORT: " + PORT);
+  console.log("Server running on PORT:", PORT);
   connectDB();
 });
